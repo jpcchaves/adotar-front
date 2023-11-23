7a73e578ce51e7ea34121ea42a83ad19da004b75
jpcchaves <joao.chaves@liferay.com>
2023-11-23 11:33:33 -0300

    fix: cherry picked header w/ theme toggle button

--- src/layouts/hydrogen/header.tsx
+++ src/layouts/hydrogen/header.tsx
@@ -13,43 +13,12 @@ import Link from 'next/link';
 
 function HeaderMenuRight() {
   return (
-    <div className="ms-auto grid shrink-0 grid-cols-4 items-center gap-2 text-gray-700 xs:gap-3 xl:gap-4">
-      <MessagesDropdown>
-        <ActionIcon
-          aria-label="Messages"
-          variant="text"
-          className="relative h-[34px] w-[34px] shadow backdrop-blur-md dark:bg-gray-100 md:h-9 md:w-9"
-        >
-          <ChatSolidIcon className="h-[18px] w-auto" />
-          <Badge
-            renderAsDot
-            color="success"
-            enableOutlineRing
-            className="absolute right-2.5 top-2.5 -translate-y-1/3 translate-x-1/2"
-          />
-        </ActionIcon>
-      </MessagesDropdown>
-      <NotificationDropdown>
-        <ActionIcon
-          aria-label="Notification"
-          variant="text"
-          className="relative h-[34px] w-[34px] shadow backdrop-blur-md dark:bg-gray-100 md:h-9 md:w-9"
-        >
-          <RingBellSolidIcon className="h-[18px] w-auto" />
-          <Badge
-            renderAsDot
-            color="warning"
-            enableOutlineRing
-            className="absolute right-2.5 top-2.5 -translate-y-1/3 translate-x-1/2"
-          />
-        </ActionIcon>
-      </NotificationDropdown>
-      <SettingsButton />
+    <div className="ms-auto grid shrink-0 grid-cols-2 items-center gap-2 text-gray-700 xs:gap-3 xl:gap-4">
+      <ThemeToggle />
       <ProfileMenu />
     </div>
   );
 }
-
 export default function Header() {
   const isMounted = useIsMounted();
   const windowScroll = useWindowScroll();
