b2f25e26f31cbce0e61fc72106859e6fd3be5cb6
jpcchaves <joao.chaves@liferay.com>
2023-11-23 11:28:23 -0300

    fix: adjusting minor issues caused by merge

--- src/layouts/helium/helium-header.tsx
+++ src/layouts/helium/helium-header.tsx
@@ -67,7 +67,7 @@ export default function Header() {
   return (
     <header
       className={
-        'sticky top-0 z-50 flex items-center bg-gray-0/80 px-4 py-4 backdrop-blur-xl dark:bg-gray-50/50 md:px-5 lg:px-6 xl:-ms-1.5 xl:pl-4 2xl:-ms-0 2xl:py-5 2xl:pl-6 3xl:px-8 3xl:pl-6 4xl:px-10 4xl:pl-9'
+        'sticky top-0 z-50 xl:-ms-1.5 2xl:-ms-0 flex items-center bg-gray-0/80 px-4 py-4 backdrop-blur-xl dark:bg-gray-50/50 md:px-5 lg:px-6 xl:pl-4 2xl:py-5 2xl:pl-6 3xl:px-8 3xl:pl-6 4xl:px-10 4xl:pl-9'
       }
     >
       <div className="flex w-full max-w-2xl items-center">
