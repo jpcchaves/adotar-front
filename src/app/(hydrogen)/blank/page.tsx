5ea6090e09d64e577b990a96c5b3b712d79ea3c7
jpcchaves <joao.chaves@liferay.com>
2023-11-24 15:40:51 -0300

    feat: add json placeholder call example in blank page

--- src/app/(hydrogen)/blank/page.tsx
+++ src/app/(hydrogen)/blank/page.tsx
@@ -30,21 +30,21 @@ export default function BlankPage() {
   const [photosList, setPhotosList] = useState<Photo[] | null>(null);
 
   useEffect(() => {
-    httpRequest<void, Photo[]>('/photos?_start=0&_limit=15', 'GET')
+    httpRequest<void, Photo[]>('/photos?_start=0&_limit=15?', 'GET')
       .then((res) => setPhotosList(res.data))
       .catch((err) => console.log(err));
   }, []);
 
   return (
-    <>
+    <div className="@container">
       <PageHeader title={pageHeader.title} breadcrumb={pageHeader.breadcrumb} />
       <>
-        <div className="grid grid-cols-1 gap-x-5 gap-y-6 ">
-          {(photosList || []).map((photo) => (
+        <div className="grid grid-cols-1 gap-x-5 gap-y-6 @container @md:grid-cols-[repeat(auto-fill,minmax(250px,1fr))] @xl:gap-x-7 @xl:gap-y-9 @4xl:grid-cols-[repeat(auto-fill,minmax(300px,1fr))] @6xl:grid-cols-[repeat(auto-fill,minmax(364px,1fr))]">
+          {(photosList || []).map((photo: Photo) => (
             <PetCard pet={photo} key={`photo-${photo.id}`} />
           ))}
         </div>
       </>
-    </>
+    </div>
   );
 }
