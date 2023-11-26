211e58165a0fb83b8b24cfd86a24470121832d51
João Paulo <jpcchaves@outlook.com>
2023-11-26 13:14:54 -0300

    feat: add export models file

---
+++ src/domain/index.ts
@@ -0,0 +1,16 @@
+import {
+  LoginRequestDTO,
+  LoginResponseDTO,
+  RegisterRequestDTO,
+} from './DTO/auth';
+
+import { ApiError, ApiMessageResponse, UserModel } from './models';
+
+export type {
+  ApiError,
+  ApiMessageResponse,
+  LoginRequestDTO,
+  LoginResponseDTO,
+  RegisterRequestDTO,
+  UserModel,
+};
