2ba26783f85bfb1303178d575a7dd94d34d192b0
João Paulo <jpcchaves@outlook.com>
2023-11-26 13:10:46 -0300

    feat: add auth dtos for login and register

---
+++ src/domain/DTO/auth/index.ts
@@ -0,0 +1,5 @@
+import { LoginRequestDTO } from './LoginRequestDTO';
+import { LoginResponseDTO } from './LoginResponseDTO';
+import { RegisterRequestDTO } from './RegisterRequestDTO';
+
+export type { LoginRequestDTO, LoginResponseDTO, RegisterRequestDTO };
