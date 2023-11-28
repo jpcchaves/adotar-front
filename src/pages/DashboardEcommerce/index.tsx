eaf460a018c52c1a4b69e7d1ad3b132846011b56
jpcchaves <joao.chaves@liferay.com>
2023-11-28 15:52:54 -0300

    feat: adjusted dashboard title

--- src/pages/DashboardEcommerce/index.tsx
+++ src/pages/DashboardEcommerce/index.tsx
@@ -11,7 +11,7 @@ import StoreVisits from "./StoreVisits";
 import TopSellers from "./TopSellers";
 
 const DashboardEcommerce = () => {
-  document.title = "Dashboard | adotar - React Admin & Dashboard Template";
+  document.title = "Adotar";
 
   const [rightColumn, setRightColumn] = useState<boolean>(true);
   const toggleRightColumn = () => {
