add2a8f037321b746cdb4b904feeda399f289ad7
jpcchaves <joao.chaves@liferay.com>
2023-11-28 15:06:46 -0300

    feat: add new project stucture

---
+++ src/pages/Projects/ProjectOverview/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Container } from "reactstrap";
+import Section from "./Section";
+
+const ProjectOverview = () => {
+  document.title =
+    "Project Overview | adotar - React Admin & Dashboard Template";
+
+  return (
+    <React.Fragment>
+      <div className="page-content">
+        <Container fluid>
+          <Section />
+        </Container>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default ProjectOverview;
