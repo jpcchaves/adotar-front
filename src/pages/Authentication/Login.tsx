00203b2e62ad5e9630af6c1f60a1913441bce8ec
jpcchaves <joao.chaves@liferay.com>
2023-11-29 16:56:17 -0300

    refactor: adjusted login page

--- src/pages/Authentication/Login.tsx
+++ src/pages/Authentication/Login.tsx
@@ -27,10 +27,11 @@ import * as Yup from "yup";
 // actions
 import { loginUser, resetLoginFlag, socialLogin } from "../../slices/thunks";
 
-import PasswordInput from "Components/Common/passwordInput";
+<<<<<<< Updated upstream
 import AuthWrapper from "pages/AuthenticationInner/AuthWrapper";
+=======
+>>>>>>> Stashed changes
 import { createSelector } from "reselect";
-import logoLight from "../../assets/images/logo-light.png";
 //import images
 
 const Login = (props: any) => {
@@ -46,7 +47,6 @@ const Login = (props: any) => {
   const { user, error, errorMsg } = useSelector(loginpageData);
 
   const [userLogin, setUserLogin] = useState<any>([]);
-  const [passwordShow, setPasswordShow] = useState<boolean>(false);
   const [loader, setLoader] = useState<boolean>(false);
 
   useEffect(() => {
@@ -100,7 +100,7 @@ const Login = (props: any) => {
     }
   }, [dispatch, errorMsg]);
 
-  document.title = "Basic SignIn | adotar - React Admin & Dashboard Template";
+  document.title = "Login | Adotar";
   return (
     <React.Fragment>
       <AuthWrapper>
@@ -111,21 +111,27 @@ const Login = (props: any) => {
                 <div className="text-center mt-sm-5 mb-4 text-white-50">
                   <div>
                     <Link to="/" className="d-inline-block auth-logo">
-                      <img src={logoLight} alt="" height="20" />
+                      <img
+                        src={"https://upload.wikimedia.org/wikipedia/commons/5/51/Paw-print.svg"}
+                        alt=""
+                        height="20"
+                      />
                     </Link>
                   </div>
-                  <p className="mt-3 fs-15 fw-medium">Premium Admin & Dashboard Template</p>
+                  <p className="mt-3 fs-15 fw-medium">Facilitando a adoção de animais</p>
                 </div>
-              </Col>
-            </Row>
+                <p className="mt-3 fs-15 fw-medium">Premium Admin & Dashboard Template</p>
+              </div>
+            </Col>
+          </Row>
 
             <Row className="justify-content-center">
               <Col md={8} lg={6} xl={5}>
                 <Card className="mt-4">
                   <CardBody className="p-4">
                     <div className="text-center mt-2">
-                      <h5 className="text-primary">Welcome Back !</h5>
-                      <p className="text-muted">Sign in to continue to adotar.</p>
+                      <h5 className="text-primary">Bem vindo!</h5>
+                      <p className="text-muted">Informe seus dados de login para acessar</p>
                     </div>
                     {error && error ? <Alert color="danger"> {error} </Alert> : null}
                     <div className="p-2 mt-4">
@@ -142,114 +148,104 @@ const Login = (props: any) => {
                             Email
                           </Label>
                           <Input
-                            name="email"
-                            className="form-control"
-                            placeholder="Enter email"
-                            type="email"
+                            name="password"
+                            value={validation.values.password || ""}
+                            type={passwordShow ? "text" : "password"}
+                            className="form-control pe-5"
+                            placeholder="Enter Password"
                             onChange={validation.handleChange}
                             onBlur={validation.handleBlur}
-                            value={validation.values.email || ""}
-                            invalid={validation.touched.email && validation.errors.email ? true : false}
+                            invalid={validation.touched.password && validation.errors.password ? true : false}
                           />
-                          {validation.touched.email && validation.errors.email ? (
-                            <FormFeedback type="invalid">{validation.errors.email}</FormFeedback>
+                          {validation.touched.password && validation.errors.password ? (
+                            <FormFeedback type="invalid">{validation.errors.password}</FormFeedback>
                           ) : null}
-                        </div>
-
-                        <div className="mb-3">
-                          <div className="float-end">
-                            <Link to="/forgot-password" className="text-muted">
-                              Forgot password?
-                            </Link>
-                          </div>
-
-                          <PasswordInput
-                            inputIdentifier="password"
-                            inputValue={validation.values.password}
-                            isRequired
-                            onBlur={validation.handleBlur}
-                            onChange={validation.handleChange}
-                            errorMessage={validation.errors.password}
-                            inputLabel="Senha"
-                            invalid={!!(validation.errors.password && validation.touched.password)}
-                          />
-                        </div>
-
-                        <div className="form-check">
-                          <Input className="form-check-input" type="checkbox" value="" id="auth-remember-check" />
-                          <Label className="form-check-label" htmlFor="auth-remember-check">
-                            Remember me
-                          </Label>
-                        </div>
-
-                        <div className="mt-4">
-                          <Button
-                            color="success"
-                            disabled={loader && true}
-                            className="btn btn-success w-100"
-                            type="submit"
+                          <button
+                            className="btn btn-link position-absolute end-0 top-0 text-decoration-none text-muted"
+                            type="button"
+                            id="password-addon"
+                            onClick={() => setPasswordShow(!passwordShow)}
                           >
-                            {loader && (
-                              <Spinner size="sm" className="me-2">
-                                {" "}
-                                Loading...{" "}
-                              </Spinner>
-                            )}
-                            Sign In
+                            <i className="ri-eye-fill align-middle"></i>
+                          </button>
+                        </div>
+                      </div>
+
+                      <div className="form-check">
+                        <Input className="form-check-input" type="checkbox" value="" id="auth-remember-check" />
+                        <Label className="form-check-label" htmlFor="auth-remember-check">
+                          Remember me
+                        </Label>
+                      </div>
+
+                      <div className="mt-4">
+                        <Button
+                          color="success"
+                          disabled={loader && true}
+                          className="btn btn-success w-100"
+                          type="submit"
+                        >
+                          {loader && (
+                            <Spinner size="sm" className="me-2">
+                              {" "}
+                              Loading...{" "}
+                            </Spinner>
+                          )}
+                          Sign In
+                        </Button>
+                      </div>
+
+                      <div className="mt-4 text-center">
+                        <div className="signin-other-title">
+                          <h5 className="fs-13 mb-4 title">Sign In with</h5>
+                        </div>
+                        <div>
+                          <Link
+                            to="#"
+                            className="btn btn-primary btn-icon me-1"
+                            onClick={(e) => {
+                              e.preventDefault();
+                              socialResponse("facebook");
+                            }}
+                          >
+                            <i className="ri-facebook-fill fs-16" />
+                          </Link>
+                          <Link
+                            to="#"
+                            className="btn btn-danger btn-icon me-1"
+                            onClick={(e) => {
+                              e.preventDefault();
+                              socialResponse("google");
+                            }}
+                          >
+                            <i className="ri-google-fill fs-16" />
+                          </Link>
+                          <Button color="dark" className="btn-icon">
+                            <i className="ri-github-fill fs-16"></i>
+                          </Button>{" "}
+                          <Button color="info" className="btn-icon">
+                            <i className="ri-twitter-fill fs-16"></i>
                           </Button>
                         </div>
+                      </div>
+                    </Form>
+                  </div>
+                </CardBody>
+              </Card>
 
-                        <div className="mt-4 text-center">
-                          <div className="signin-other-title">
-                            <h5 className="fs-13 mb-4 title">Sign In with</h5>
-                          </div>
-                          <div>
-                            <Link
-                              to="#"
-                              className="btn btn-primary btn-icon me-1"
-                              onClick={(e) => {
-                                e.preventDefault();
-                                socialResponse("facebook");
-                              }}
-                            >
-                              <i className="ri-facebook-fill fs-16" />
-                            </Link>
-                            <Link
-                              to="#"
-                              className="btn btn-danger btn-icon me-1"
-                              onClick={(e) => {
-                                e.preventDefault();
-                                socialResponse("google");
-                              }}
-                            >
-                              <i className="ri-google-fill fs-16" />
-                            </Link>
-                            <Button color="dark" className="btn-icon">
-                              <i className="ri-github-fill fs-16"></i>
-                            </Button>{" "}
-                            <Button color="info" className="btn-icon">
-                              <i className="ri-twitter-fill fs-16"></i>
-                            </Button>
-                          </div>
-                        </div>
-                      </Form>
-                    </div>
-                  </CardBody>
-                </Card>
-
-                <div className="mt-4 text-center">
-                  <p className="mb-0">
-                    Don't have an account ?{" "}
-                    <Link to="/register" className="fw-semibold text-primary text-decoration-underline">
-                      {" "}
-                      Signup{" "}
-                    </Link>{" "}
-                  </p>
-                </div>
-              </Col>
-            </Row>
-          </Container>
-        </div>
+              <div className="mt-4 text-center">
+                <p className="mb-0">
+                  Don't have an account ?{" "}
+                  <Link to="/register" className="fw-semibold text-primary text-decoration-underline">
+                    {" "}
+                    Signup{" "}
+                  </Link>{" "}
+                </p>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
       </AuthWrapper>
     </React.Fragment>
   );
