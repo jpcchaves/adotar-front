1c18ed0ddf5b77798b07ee14d422d7cd46d8a4f0
João Paulo <jpcchaves@outlook.com>
2023-12-03 19:05:34 -0300

    refactor: adjusted useLogin to implement new http methods

--- src/modules/auth/hooks/useLogin.tsx
+++ src/modules/auth/hooks/useLogin.tsx
@@ -5,6 +5,9 @@ import { useNavigation } from "hooks/navigate/useNavigation";
 import { useAppDispatch } from "hooks/redux/useRedux";
 import { loadAuthError, loadClearError } from "slices/auth/login/reducer";
 import { UserModel } from "../../../domain/models/user/UserModel";
+import { HttpMethod, httpRequest } from "../../../utils/http";
+import { LoginResponseDTO } from "../../../domain/DTO/auth/LoginResponseDTO";
+import { REACT_APP_API_LOGIN_ENDPOINT, REACT_APP_API_LOGIN_V2 } from "../../../contants/env";
 
 interface IUseLogin {
   isLoading: boolean;
@@ -33,21 +36,24 @@ const useLogin = (): IUseLogin => {
 
   const login = async (loginRequestDTO: LoginRequestDTO) => {
     setLoading(true);
-    try {
-      const data = {} as UserModel;
-      sessionStorage.setItem("authUser", JSON.stringify(data));
-      handleRememberUser(loginRequestDTO);
-
-      notify("Usuario autenticado com sucesso!", "success");
-
-      navigateTo("/pets");
-    } catch (error: any & string) {
-      dispatch(loadAuthError({ errorMessage: error, hasError: true }));
-      handleClearErrorMessage();
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
+    await httpRequest<LoginRequestDTO, LoginResponseDTO>(
+      HttpMethod.POST,
+      `${REACT_APP_API_LOGIN_V2}/${REACT_APP_API_LOGIN_ENDPOINT}`,
+      loginRequestDTO,
+    )
+      .then((data: LoginResponseDTO) => {
+        sessionStorage.setItem("authUser", JSON.stringify(data));
+        handleRememberUser(loginRequestDTO);
+        notify("Usuario autenticado com sucesso!", "success");
+        navigateTo("/pets");
+      })
+      .catch((err: string) => {
+        dispatch(loadAuthError({ errorMessage: err, hasError: true }));
+        handleClearErrorMessage();
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   };
 
   return { login, isLoading };
