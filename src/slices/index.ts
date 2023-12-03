76144df7d8cd9fd591bf7790eaa45341b25ef5e4
João Paulo <jpcchaves@outlook.com>
2023-12-03 18:45:25 -0300

    refactor: remove unused slices

--- src/slices/index.ts
+++ src/slices/index.ts
@@ -5,17 +5,13 @@ import LayoutReducer from "./layouts/reducer";
 
 // Authentication
 import { store } from "index";
-import ForgetPasswordReducer from "./auth/forgetpwd/reducer";
 import AuthReducer from "./auth/login/reducer";
-import ProfileReducer from "./auth/profile/reducer";
 import AccountReducer from "./auth/register/reducer";
 
 const rootReducer = combineReducers({
   Layout: LayoutReducer,
   Auth: AuthReducer,
   Account: AccountReducer,
-  ForgetPassword: ForgetPasswordReducer,
-  Profile: ProfileReducer,
 });
 
 export default rootReducer;
