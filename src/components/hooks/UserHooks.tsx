1115fde09a8756e8afa01d4d8d58b3b693e27083
João Paulo <jpcchaves@outlook.com>
2023-12-03 18:47:58 -0300

    refactor: optimizing code and simplifying code

--- src/components/hooks/UserHooks.tsx
+++ src/components/hooks/UserHooks.tsx
@@ -1,17 +1,17 @@
 import { useEffect, useState } from "react";
-import { getLoggedinUser } from "../../helpers/api_helper";
+import {getLoggedinUser} from "../../utils/http";
 
 const useProfile = () => {
   const userProfileSession = getLoggedinUser();
   const token = userProfileSession && userProfileSession["accessToken"];
-  const [loading, setLoading] = useState(userProfileSession ? false : true);
+  const [loading, setLoading] = useState(!userProfileSession);
   const [userProfile, setUserProfile] = useState(userProfileSession ? userProfileSession : null);
 
   useEffect(() => {
     const userProfileSession = getLoggedinUser();
     const token = userProfileSession && userProfileSession["accessToken"];
     setUserProfile(userProfileSession ? userProfileSession.user : null);
-    setLoading(token ? false : true);
+    setLoading(!token);
   }, []);
 
   return { userProfile, loading, token };
