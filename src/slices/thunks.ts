0d69852568e792af8e7fe029ef5fac775440987e
João Paulo <jpcchaves@outlook.com>
2023-12-03 18:44:17 -0300

    refactor: removing unused thunks and slices

--- src/slices/thunks.ts
+++ src/slices/thunks.ts
@@ -1,9 +1,2 @@
 // Front
 export * from "./layouts/thunk";
-
-// Authentication
-export * from "./auth/forgetpwd/thunk";
-export * from "./auth/profile/thunk";
-
-// Dashboard Ecommerce
-export * from "./dashboardEcommerce/thunk";
