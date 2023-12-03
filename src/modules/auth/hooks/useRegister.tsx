70d27368f66efd737a548cf29d6c1dd68e1955b1
João Paulo <jpcchaves@outlook.com>
2023-12-03 15:15:38 -0300

    feat: add validation prop in useRegister hook to reset form when successfully submitted

--- src/modules/auth/hooks/useRegister.tsx
+++ src/modules/auth/hooks/useRegister.tsx
@@ -1,17 +1,22 @@
 import { notify } from "components/common/notify";
 import { RegisterRequestDTO } from "domain/DTO/auth/RegisterRequestDTO";
+import { FormikValues } from "formik";
 import { postRegister } from "helpers/fakebackend_helper";
 import useLoading from "hooks/loading/useLoading";
 import { useNavigation } from "hooks/navigate/useNavigation";
 import { useAppDispatch } from "hooks/redux/useRedux";
 import { loadUserEmail } from "slices/auth/login/reducer";
 
+interface IProps {
+  validation: FormikValues;
+}
+
 interface IUseRegister {
   register: (registerRequestDto: RegisterRequestDTO) => void;
   isLoading: boolean;
 }
 
-const useRegister = (): IUseRegister => {
+const useRegister = ({ validation }: IProps): IUseRegister => {
   const { isLoading, setLoading } = useLoading();
   const { navigateWithTimer } = useNavigation();
   const dispatch = useAppDispatch();
@@ -23,6 +28,7 @@ const useRegister = (): IUseRegister => {
       notify("Usuário cadastrado com sucesso! Você será redirecionado para a tela de login!", "success");
       navigateWithTimer("/entrar", 2000);
       dispatch(loadUserEmail({ email: registerRequestDto.email }));
+      validation.resetForm();
     } catch (error: any & string) {
       // console.log(error);
     } finally {
