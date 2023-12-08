3af356b8624b068a6d5f77fbcb32ad8451ce2bed
João Paulo <jpcchaves@outlook.com>
2023-12-08 00:28:22 -0300

    refactor: import endpoints from env file

--- src/configs/auth.ts
+++ src/configs/auth.ts
@@ -1,7 +1,18 @@
+const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL
+const verifyTokenV1 = process.env.NEXT_PUBLIC_API_VERIFY_TOKEN_V1
+const verifyTokenEndpoint = process.env.NEXT_PUBLIC_API_VERIFY_TOKEN_ENDPOINT
+
+const loginV2 = process.env.NEXT_PUBLIC_API_LOGIN_V2
+const loginEndpoint = process.env.NEXT_PUBLIC_API_LOGIN_ENDPOINT
+const registerV1 = process.env.NEXT_PUBLIC_API_REGISTER_V1
+const registerEndpoint = process.env.NEXT_PUBLIC_API_REGISTER_ENDPOINT
+
+const storageTokenKeyName = process.env.NEXT_PUBLIC_STORAGE_TOKEN_KEY_NAME
+
 export default {
-  meEndpoint: '/auth/me',
-  loginEndpoint: '/jwt/login',
-  registerEndpoint: '/jwt/register',
-  storageTokenKeyName: 'accessToken',
-  onTokenExpiration: 'refreshToken' // logout | refreshToken
+  meEndpoint: `${baseUrl}/${verifyTokenV1}/${verifyTokenEndpoint}`,
+  loginEndpoint: `${baseUrl}/${loginV2}/${loginEndpoint}`,
+  registerEndpoint: `${baseUrl}/${registerV1}/${registerEndpoint}`,
+  storageTokenKeyName: `${storageTokenKeyName}`,
+  onTokenExpiration: 'logout' // logout | refreshToken
 }
