b1453e697cc9cdca1d480f8a9d279581f8f7ff33
João Paulo <jpcchaves@outlook.com>
2023-12-09 14:51:10 -0300

    feat: create component auth wrapper

---
+++ src/@core/components/authWrapper/index.tsx
@@ -0,0 +1,115 @@
+import FooterIllustrationsV1 from 'src/views/pages/auth/FooterIllustrationsV1'
+
+// ** React Imports
+
+// ** Next Import
+
+// ** MUI Components
+import Box from '@mui/material/Box'
+import MuiCard, { CardProps } from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Typography from '@mui/material/Typography'
+import { styled, useTheme } from '@mui/material/styles'
+
+import themeConfig from 'src/configs/themeConfig'
+
+const Card = styled(MuiCard)<CardProps>(({ theme }) => ({
+  [theme.breakpoints.up('sm')]: { width: '40rem' },
+  [theme.breakpoints.down('sm')]: { width: '32rem' }
+}))
+
+interface IProps {
+  children: JSX.Element
+}
+
+const AuthWrapperLayout = ({ children }: IProps) => {
+  const theme = useTheme()
+
+  return (
+    <Box className='content-center'>
+      <Card sx={{ zIndex: 1 }}>
+        <CardContent sx={{ p: theme => `${theme.spacing(15.5, 7, 6.5)} !important` }}>
+          <Box sx={{ mb: 8, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <svg width={47} fill='none' height={26} viewBox='0 0 268 150' xmlns='http://www.w3.org/2000/svg'>
+              <rect
+                rx='25.1443'
+                width='50.2886'
+                height='143.953'
+                fill={theme.palette.primary.main}
+                transform='matrix(-0.865206 0.501417 0.498585 0.866841 195.571 0)'
+              />
+              <rect
+                rx='25.1443'
+                width='50.2886'
+                height='143.953'
+                fillOpacity='0.4'
+                fill='url(#paint0_linear_7821_79167)'
+                transform='matrix(-0.865206 0.501417 0.498585 0.866841 196.084 0)'
+              />
+              <rect
+                rx='25.1443'
+                width='50.2886'
+                height='143.953'
+                fill={theme.palette.primary.main}
+                transform='matrix(0.865206 0.501417 -0.498585 0.866841 173.147 0)'
+              />
+              <rect
+                rx='25.1443'
+                width='50.2886'
+                height='143.953'
+                fill={theme.palette.primary.main}
+                transform='matrix(-0.865206 0.501417 0.498585 0.866841 94.1973 0)'
+              />
+              <rect
+                rx='25.1443'
+                width='50.2886'
+                height='143.953'
+                fillOpacity='0.4'
+                fill='url(#paint1_linear_7821_79167)'
+                transform='matrix(-0.865206 0.501417 0.498585 0.866841 94.1973 0)'
+              />
+              <rect
+                rx='25.1443'
+                width='50.2886'
+                height='143.953'
+                fill={theme.palette.primary.main}
+                transform='matrix(0.865206 0.501417 -0.498585 0.866841 71.7728 0)'
+              />
+              <defs>
+                <linearGradient
+                  y1='0'
+                  x1='25.1443'
+                  x2='25.1443'
+                  y2='143.953'
+                  id='paint0_linear_7821_79167'
+                  gradientUnits='userSpaceOnUse'
+                >
+                  <stop />
+                  <stop offset='1' stopOpacity='0' />
+                </linearGradient>
+                <linearGradient
+                  y1='0'
+                  x1='25.1443'
+                  x2='25.1443'
+                  y2='143.953'
+                  id='paint1_linear_7821_79167'
+                  gradientUnits='userSpaceOnUse'
+                >
+                  <stop />
+                  <stop offset='1' stopOpacity='0' />
+                </linearGradient>
+              </defs>
+            </svg>
+            <Typography variant='h6' sx={{ ml: 2, lineHeight: 1, fontWeight: 700, fontSize: '1.5rem !important' }}>
+              {themeConfig.templateName}
+            </Typography>
+          </Box>
+          {children}
+        </CardContent>
+      </Card>
+      <FooterIllustrationsV1 image={`/images/pages/auth-v1-register-mask-${theme.palette.mode}.png`} />
+    </Box>
+  )
+}
+
+export default AuthWrapperLayout
