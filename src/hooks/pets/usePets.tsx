929c6f434ec7a66d4f7b6b0298cac3a174f29d99
João Paulo <jpcchaves@outlook.com>
2023-12-09 21:34:35 -0300

    feat: add usepets hook

---
+++ src/hooks/pets/usePets.tsx
@@ -0,0 +1,31 @@
+import { ApiResponsePaginated } from 'src/domain/models/ApiResponsePaginated'
+import { PetModelMin } from 'src/domain/models/pet/PetModel'
+import { HttpMethod, httpRequest } from 'src/utils/http'
+import { useAppDispatch } from '../useRedux'
+
+import { loadPetsPaginated } from 'src/store/pets'
+import petsRoutes from '../../configs/routes/pets'
+import useLoading from '../loading/useLoading'
+
+const usePets = () => {
+  const dispatch = useAppDispatch()
+  const { isLoading, setLoading } = useLoading()
+
+  const getListPets = async () => {
+    setLoading(true)
+    await httpRequest<void, ApiResponsePaginated<PetModelMin>>(HttpMethod.GET, petsRoutes.petsEndpoint)
+      .then(res => {
+        dispatch(loadPetsPaginated(res))
+      })
+      .catch(() => {
+        // console.log(err);
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }
+
+  return { getListPets, isLoading }
+}
+
+export default usePets
