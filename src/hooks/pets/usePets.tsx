5bf1092cc561fda9b8a2d9b40dcfb5bf744c1ec8
jpcchaves <joao.chaves@liferay.com>
2023-12-11 13:52:55 -0300

    feat: add mthod to handle saved pets, favorite and infinite scroll

--- src/hooks/pets/usePets.tsx
+++ src/hooks/pets/usePets.tsx
@@ -1,21 +1,31 @@
 import { ApiResponsePaginated } from 'src/domain/models/ApiResponsePaginated'
 import { PetModelMin } from 'src/domain/models/pet/PetModel'
 import { HttpMethod, httpRequest } from 'src/utils/http'
-import { useAppDispatch } from '../useRedux'
+import { useAppDispatch, useAppSelector } from '../useRedux'
 
-import { loadPetsPaginated } from 'src/store/pets'
+import toast from 'react-hot-toast'
+import { ApiMessageResponse } from 'src/domain/models/ApiMessageResponse'
+import { loadPets, loadPetsPaginated } from 'src/store/pets'
+import { updatePetFavorite } from 'src/utils/pet/updatePetFavorite'
 import petsRoutes from '../../configs/routes/pets'
 import useLoading from '../loading/useLoading'
+
+const FAVORITE = true
+const NOT_FAVORITE = false
 
 const usePets = () => {
   const dispatch = useAppDispatch()
   const { isLoading, setLoading } = useLoading()
+  const { pets } = useAppSelector(state => state.pets)
 
-  const getListPets = async () => {
+  const getListPets = async (page: string) => {
     setLoading(true)
-    await httpRequest<void, ApiResponsePaginated<PetModelMin>>(HttpMethod.GET, `${petsRoutes.petsEndpoint}?size=6`)
+    await httpRequest<void, ApiResponsePaginated<PetModelMin>>(
+      HttpMethod.GET,
+      `${petsRoutes.petsEndpoint}?page=${page}&size=6`
+    )
       .then(res => {
-        dispatch(loadPetsPaginated(res))
+        handlePetListPagination(res)
       })
       .catch(() => {
         // console.log(err);
@@ -25,7 +35,56 @@ const usePets = () => {
       })
   }
 
-  return { getListPets, isLoading }
+  const addSavedPet = async (petId: string) => {
+    await httpRequest<void, ApiMessageResponse>(HttpMethod.POST, `${petsRoutes.savedPetsEndpoint}/${petId}`)
+      .then(({ message }) => {
+        toast.success(message)
+        handleUpdatePetFavorite(petId, FAVORITE)
+      })
+      .catch(err => {
+        toast.error(err)
+
+        // console.log(err);
+      })
+  }
+
+  const removeSavedPet = async (petId: string) => {
+    await httpRequest<void, ApiMessageResponse>(HttpMethod.DELETE, `${petsRoutes.savedPetsEndpoint}/${petId}`)
+      .then(({ message }) => {
+        toast.success(message)
+        handleUpdatePetFavorite(petId, NOT_FAVORITE)
+      })
+      .catch(err => {
+        toast.error(err)
+
+        // console.log(err);
+      })
+  }
+
+  const handlePetListPagination = (petsPaginated: ApiResponsePaginated<PetModelMin>) => {
+    if (pets && petsPaginated.pageNo > 0) {
+      const newData = pets.concat(petsPaginated.content)
+      dispatch(
+        loadPetsPaginated({
+          content: newData,
+          last: petsPaginated.last,
+          totalElements: petsPaginated.totalElements,
+          pageSize: petsPaginated.pageSize,
+          pageNo: petsPaginated.pageNo,
+          totalPages: petsPaginated.totalPages
+        })
+      )
+    } else {
+      dispatch(loadPetsPaginated(petsPaginated))
+    }
+  }
+
+  const handleUpdatePetFavorite = (petId: string, newFavoriteValue: boolean) => {
+    updatePetFavorite(pets!, petId, newFavoriteValue)
+    dispatch(loadPets(updatePetFavorite(pets!, petId, newFavoriteValue)))
+  }
+
+  return { getListPets, addSavedPet, removeSavedPet, handlePetListPagination, isLoading }
 }
 
 export default usePets
