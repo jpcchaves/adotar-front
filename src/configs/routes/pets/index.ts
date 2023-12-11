e754121bdae9063bc7b13dc6848a37d818546201
jpcchaves <joao.chaves@liferay.com>
2023-12-11 13:51:59 -0300

    feat: add saved pets endpoint

--- src/configs/routes/pets/index.ts
+++ src/configs/routes/pets/index.ts
@@ -1,6 +1,10 @@
 const petsV2 = process.env.NEXT_PUBLIC_API_PETS_V2
 const petsEndpoint = process.env.NEXT_PUBLIC_API_PETS_ENDPOINT
 
+const savedPetsV1 = process.env.NEXT_PUBLIC_API_SAVED_PETS_V1
+const savedPetsEndpoint = process.env.NEXT_PUBLIC_API_SAVED_PETS_ENDPOINT
+
 export default {
-  petsEndpoint: `/${petsV2}/${petsEndpoint}`
+  petsEndpoint: `/${petsV2}/${petsEndpoint}`,
+  savedPetsEndpoint: `/${savedPetsV1}/${savedPetsEndpoint}`
 }
