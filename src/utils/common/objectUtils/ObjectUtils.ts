25fa8b5c0741a7a769fe88f941c427aedb49f21f
João Paulo <jpcchaves@outlook.com>
2023-12-19 21:41:55 -0300

    feat: add method has all keys to verify if an object has all keys passed

--- src/utils/common/objectUtils/ObjectUtils.ts
+++ src/utils/common/objectUtils/ObjectUtils.ts
@@ -4,4 +4,8 @@ export class ObjectUtils {
   static isEmpty(obj: object): boolean {
     return Object.keys(obj).length === this.isEmptyObjectLength
   }
+
+  static hasAllKeys(object: Record<string, any>, keysToVerify: string[]): boolean {
+    return keysToVerify.every(key => object.hasOwnProperty(key))
+  }
 }
