8eb5ba1325405d8827ea7c69149c85ecfe254b69
João Paulo <jpcchaves@outlook.com>
2023-12-19 21:42:19 -0300

    feat: converted objects into methods to make it more flexible

--- src/views/modules/pets/data/formInitialValues/index.ts
+++ src/views/modules/pets/data/formInitialValues/index.ts
@@ -1,25 +1,25 @@
-export const stepOneInitialValues = {
-  typeId: '',
-  name: '',
-  gender: '',
-  size: '',
-  healthCondition: ''
+export const stepOneInitialValues = () => {
+  return {
+    typeId: '',
+    name: '',
+    gender: '',
+    size: '',
+    healthCondition: ''
+  }
 }
 
-export const stepTwoInitialValues = {
-  breedId: '',
-  color: '',
-  yearsAge: '',
-  monthsAge: '',
-  description: '',
-  characteristicsIds: null
+export const stepTwoInitialValues = () => {
+  return { breedId: '', color: '', yearsAge: '', monthsAge: '', description: '', characteristicsIds: null }
 }
 
-export const stepThreeInitialValues = {
-  zipcode: '',
-  street: '',
-  number: '',
-  complement: '',
-  neighborhood: '',
-  cityId: ''
+export const stepThreeInitialValues = () => {
+  return { zipcode: '', street: '', number: '', complement: '', neighborhood: '', cityId: '' }
+}
+
+export const getFormInitialValues = () => {
+  return {
+    ...stepOneInitialValues(),
+    ...stepTwoInitialValues(),
+    ...stepThreeInitialValues()
+  }
 }
