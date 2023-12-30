899ce90a365222c7658d725237bb3d20fdfe1008
João Chaves <joao.chaves@liferay.com>
2023-12-30 08:57:30 -0300

    feat: create component for input file

---
+++ src/@core/components/inputs/components/inputFile/style.ts
@@ -0,0 +1,26 @@
+import Typography, { TypographyProps } from '@mui/material/Typography'
+import { styled } from '@mui/material/styles'
+
+// Styled component for the upload image inside the dropzone area
+const Img = styled('img')(({ theme }) => ({
+  [theme.breakpoints.up('md')]: {
+    marginRight: theme.spacing(10)
+  },
+  [theme.breakpoints.down('md')]: {
+    marginBottom: theme.spacing(4)
+  },
+  [theme.breakpoints.down('sm')]: {
+    width: 250
+  },
+  userSelect: 'none'
+}))
+
+// Styled component for the heading inside the dropzone area
+const HeadingTypography = styled(Typography)<TypographyProps>(({ theme }) => ({
+  marginBottom: theme.spacing(5),
+  [theme.breakpoints.down('sm')]: {
+    marginBottom: theme.spacing(4)
+  }
+}))
+
+export { Img, HeadingTypography }
