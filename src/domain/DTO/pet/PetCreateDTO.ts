93638f7cacdf14cb4d9586775ada335aa33eee0a
João Chaves <joao.chaves@liferay.com>
2024-01-07 12:50:07 -0300

    feat: create pet create dto interface

---
+++ src/domain/DTO/pet/PetCreateDTO.ts
@@ -0,0 +1,21 @@
+export interface PetCreateDTO {
+  breedId: string
+  characteristicsIds: string[]
+  cityId: string
+  color: string
+  complement: string
+  description: string
+  gender: string
+  healthCondition: string
+  monthsAge: string
+  name: string
+  neighborhood: string
+  number: string
+  petPictures: { imgUrl: string }[]
+  size: string
+  state: string
+  street: string
+  typeId: string
+  yearsAge: string
+  zipcode: string
+}
