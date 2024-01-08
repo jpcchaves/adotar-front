fee0c2b39f11f8b80ed3a966bed92288bfbf5894
jpcchaves <joao.chaves@liferay.com>
2024-01-08 09:34:33 -0300

    feat: add method navigate back delayed

--- src/hooks/navigation/useNavigation.tsx
+++ src/hooks/navigation/useNavigation.tsx
@@ -1,19 +1,31 @@
 import { useRouter } from 'next/router'
 
-const useNavigation = () => {
+interface IUseNavigation {
+  navigate: (path: string) => void
+  navigateWithTime: (path: string, time: number) => void
+  navigateBackDelayed: (time: number) => void
+}
+
+const useNavigation = (): IUseNavigation => {
   const router = useRouter()
 
   const navigate = (path: string) => {
-    router.replace(path)
+    router.push(path)
   }
 
   const navigateWithTime = (path: string, time: number) => {
     setTimeout(() => {
-      router.replace(path)
+      router.push(path)
     }, time)
   }
 
-  return { navigate, navigateWithTime }
+  const navigateBackDelayed = (time: number) => {
+    setTimeout(() => {
+      router.back()
+    }, time)
+  }
+
+  return { navigate, navigateWithTime, navigateBackDelayed }
 }
 
 export default useNavigation
