aa33108decafab7297406186325e3af6bd2af8a8
jpcchaves <joao.chaves@liferay.com>
2024-01-17 15:08:22 -0300

    feat: add route for user pets

--- src/configs/routes/index.ts
+++ src/configs/routes/index.ts
@@ -1,3 +1,3 @@
-import { myPetsEndpointV1, petsEndpointV2, savedPetsEndpointV1 } from './pets'
+import { myPetsEndpointV1, petsEndpointV2, savedPetsEndpointV1, petDetailsEndpointV1 } from './pets'
 
-export { myPetsEndpointV1, petsEndpointV2, savedPetsEndpointV1 }
+export { myPetsEndpointV1, petsEndpointV2, savedPetsEndpointV1, petDetailsEndpointV1 }
--- src/configs/routes/pets/index.ts
+++ src/configs/routes/pets/index.ts
@@ -1,3 +1,4 @@
+export const petDetailsEndpointV1 = '/v1/pets/pet-details'
 export const petsEndpointV2 = `/v2/pets`
 export const savedPetsEndpointV1 = `/v1/saved-pets`
 export const myPetsEndpointV1 = '/v1/pets/by-user'
