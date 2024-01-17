9db5cebe0ec2c660e18fa168333d2900a971b61c
jpcchaves <joao.chaves@liferay.com>
2024-01-17 15:08:09 -0300

    feat: add pet details dto model

---
+++ src/domain/DTO/pet/PetDetailsDTO.ts
@@ -0,0 +1,20 @@
+export type PetDetailsDTO = {
+  id: number
+  name: string
+  yearsAge: number
+  monthsAge: number
+  gender: string
+  size: string
+  healthCondition: string
+  color: string
+  description: string
+  visualizations: number
+  adoptionDate: null
+  active: boolean
+  characteristics: number[]
+  type: string
+  breed: string
+  petPictures: string[]
+  available: boolean
+  favorite: boolean
+}
