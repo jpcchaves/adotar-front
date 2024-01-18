7e5b021473543701329d30547c035973d407f1a2
jpcchaves <joao.chaves@liferay.com>
2024-01-18 15:49:18 -0300

    refactor: adjusting form initial values to verify if pet details exist

--- src/views/modules/pets/data/formInitialValues/index.ts
+++ src/views/modules/pets/data/formInitialValues/index.ts
@@ -1,78 +1,76 @@
-export const stepOneInitialValues = () => {
+import { PetDetailsDTO } from 'src/domain/DTO/pet/PetDetailsDTO'
+
+export const stepOneInitialValues = (petDetails: PetDetailsDTO) => {
   return {
-    typeId: {
-      value: '',
-      label: ''
-    },
-    name: '',
+    typeId: { value: petDetails ? String(petDetails.type.id) : '' },
+    name: petDetails ? petDetails?.name : '',
     gender: {
-      value: '',
-      label: ''
+      value: petDetails ? petDetails.gender : ''
     },
     size: {
-      value: '',
-      label: ''
+      value: petDetails ? petDetails?.size : ''
     },
     healthCondition: {
-      value: '',
-      label: ''
+      value: petDetails ? petDetails.healthCondition : ''
     }
   }
 }
 
-export const stepTwoInitialValues = () => {
+export const stepTwoInitialValues = (petDetails: PetDetailsDTO) => {
   return {
     breedId: {
-      value: '',
-      label: ''
+      value: petDetails ? petDetails.breed.id : '',
+      label: petDetails ? petDetails.breed.name : ''
     },
-    color: '',
+    color: petDetails ? petDetails.color : '',
     yearsAge: {
-      value: '',
-      label: ''
+      value: petDetails ? petDetails.yearsAge : ''
     },
     monthsAge: {
-      value: '',
-      label: ''
+      value: petDetails ? petDetails.monthsAge : ''
     },
-    description: '',
-    characteristicsIds: []
+    description: petDetails ? petDetails.description : '',
+    characteristicsIds: petDetails ? petDetails.characteristics.map(c => String(c)) : []
   }
 }
 
-export const stepThreeInitialValues = () => {
+export const stepThreeInitialValues = (petDetails: PetDetailsDTO) => {
   return {
-    zipcode: '',
+    zipcode: petDetails ? petDetails.address.zipcode : '',
+
     state: {
-      value: '',
-      label: ''
+      value: petDetails ? petDetails.address.state : ''
     },
     city: {
-      value: '',
-      label: ''
+      value: petDetails ? petDetails.address.city : ''
     },
-    street: '',
-    number: '',
-    complement: '',
-    neighborhood: ''
+    street: petDetails ? petDetails.address.street : '',
+    number: petDetails ? petDetails.address.number : '',
+    complement: petDetails ? petDetails.address.complement : '',
+    neighborhood: petDetails ? petDetails.address.neighborhood : ''
   }
 }
 
-export const stepFourInitialValues = () => {
+export const stepFourInitialValues = (petDetails: PetDetailsDTO) => {
   return {
-    petPictures: []
+    petPictures: petDetails ? petDetails.petPictures : []
   }
 }
 
-export const getFormInitialValues = () => {
+export const getFormInitialValues = (petDetails: PetDetailsDTO) => {
   return {
-    ...stepOneInitialValues(),
-    ...stepTwoInitialValues(),
-    ...stepThreeInitialValues(),
-    ...stepFourInitialValues()
+    ...stepOneInitialValues(petDetails),
+    ...stepTwoInitialValues(petDetails),
+    ...stepThreeInitialValues(petDetails),
+    ...stepFourInitialValues(petDetails)
   }
 }
 
-export const getFormInitialValuesArr = () => {
-  return [stepOneInitialValues(), stepTwoInitialValues(), stepThreeInitialValues(), stepFourInitialValues()]
+export const getFormInitialValuesArr = (petDetails: PetDetailsDTO) => {
+  return [
+    stepOneInitialValues(petDetails),
+    stepTwoInitialValues(petDetails),
+    stepThreeInitialValues(petDetails),
+    stepFourInitialValues(petDetails)
+  ]
 }
