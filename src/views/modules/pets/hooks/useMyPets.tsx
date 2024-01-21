10991ba7334a5c702c1a606fe2814d3a8ee007da
João Chaves <joao.chaves@liferay.com>
2024-01-21 14:30:36 -0300

    feat: add necessary props to perform delete pet action also add close menu when a menuitem is clicked

--- src/views/modules/pets/hooks/useMyPets.tsx
+++ src/views/modules/pets/hooks/useMyPets.tsx
@@ -1,3 +1,4 @@
+import toast from 'react-hot-toast'
 import { useDispatch } from 'react-redux'
 import {} from 'src/configs/routes/pets'
 import { ApiResponsePaginated } from 'src/domain/models/ApiResponsePaginated'
@@ -5,8 +6,14 @@ import { PetModelMin } from 'src/domain/models/pet/PetModel'
 import useLoading from 'src/hooks/loading/useLoading'
 import { loadMyPets } from 'src/store/pets'
 import { HttpMethod, httpRequest } from 'src/utils/http'
+import { petService } from '../service/impl/PetServiceImpl'
 
-const useMyPets = () => {
+interface IProps {
+  toggleDeleteModal: () => void
+  setSelectedPetId: (prevState: string) => void
+}
+
+const useMyPets = ({ toggleDeleteModal, setSelectedPetId }: IProps) => {
   const dispatch = useDispatch()
   const { setLoading, isLoading } = useLoading()
   const getMyPets = async () => {
@@ -18,6 +25,7 @@ const useMyPets = () => {
     )
       .then(res => {
         dispatch(loadMyPets(res.content))
+        setSelectedPetId('')
       })
       .catch(err => {
         console.log(err)
@@ -27,7 +35,26 @@ const useMyPets = () => {
       })
   }
 
-  return { getMyPets, isLoading }
+  const deletePet = async (id: string) => {
+    setLoading(true)
+
+    await petService
+      .deletePet(id)
+      .then(res => {
+        toast.success(res.message)
+        getMyPets()
+        toggleDeleteModal()
+      })
+      .catch(err => {
+        toast.error(err)
+      })
+      .finally(() => {
+        setLoading(false)
+        setSelectedPetId('')
+      })
+  }
+
+  return { getMyPets, deletePet, isLoading }
 }
 
 export default useMyPets
