58fa68d0deea8350eff2d9146bd694bb3bf5d15f
jpcchaves <joao.chaves@liferay.com>
2024-01-22 14:42:02 -0300

    fix: adjusting breadcrumb to deal with dynamic routes

--- src/layouts/components/Breadcrumb.tsx
+++ src/layouts/components/Breadcrumb.tsx
@@ -17,7 +17,7 @@ const DYNAMIC_ROUTE_IDENTIFIER = '['
 const Breadcrumb = () => {
   const { pathname } = useRouter()
 
-  const pathnames = pathname.split('/').filter(x => x)
+  const pathnames = pathname.split('/').filter(x => !x.includes(DYNAMIC_ROUTE_IDENTIFIER) && x)
 
   const breadcumbsMap: { [key: string]: string } = {
     '/pets': 'Pets',
@@ -29,12 +29,10 @@ const Breadcrumb = () => {
   return (
     <Box pl={2} mb={5}>
       <Breadcrumbs aria-label='breadcrumb' maxItems={2}>
-        {pathnames.length > 0 &&
-          pathnames.map((path, index) => {
+        {pathnames.length &&
+          pathnames.map((_, index) => {
             const last = index === pathnames.length - 1
             const to = `/${pathnames.slice(0, index + 1).join('/')}`
-
-            if (path.startsWith(DYNAMIC_ROUTE_IDENTIFIER)) return
 
             return last ? (
               <Typography style={{ color: 'white' }} key={to}>
