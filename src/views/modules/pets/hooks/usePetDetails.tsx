d9f5e21a84ff3c54bbc43c1bb0a002c0208c3494
João Chaves <joao.chaves@liferay.com>
2024-01-31 22:04:57 -0300

    feat: implemented method to generate pet card pdf

--- src/views/modules/pets/hooks/usePetDetails.tsx
+++ src/views/modules/pets/hooks/usePetDetails.tsx
@@ -2,6 +2,7 @@ import { useCallback } from 'react'
 import useLoading from 'src/hooks/loading/useLoading'
 import { useAppDispatch } from 'src/hooks/useRedux'
 import { loadPetById } from 'src/store/pets'
+import { HttpMethod, httpRequest } from 'src/utils/http'
 import { petDetailsService } from '../service/impl/PetDetailsServiceImpl'
 
 const usePetDetails = () => {
@@ -24,7 +25,27 @@ const usePetDetails = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  return { getPetDetailedInfo, isLoading }
+  const generatePetCardPdf = async (petId: string | undefined) => {
+    await httpRequest<void, Blob>(HttpMethod.GET, `/v1/pets/generate-card/${petId}`, undefined, 'blob')
+      .then(res => {
+        openPdfInNewTab(generatePdfUrl(generateBlob(res)))
+      })
+      .catch(err => console.log(err))
+  }
+
+  const generateBlob = (data: Blob) => {
+    return new Blob([data], { type: 'application/pdf' })
+  }
+
+  const generatePdfUrl = (data: Blob) => {
+    return URL.createObjectURL(generateBlob(data))
+  }
+
+  const openPdfInNewTab = (pdfUrl: string) => {
+    window.open(pdfUrl, '_blank')
+  }
+
+  return { getPetDetailedInfo, generatePetCardPdf, isLoading }
 }
 
 export default usePetDetails
