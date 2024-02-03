b141850d45cc99e5333a5e1b1bb7f1a3ce390dd6
João Chaves <joao.chaves@liferay.com>
2024-02-03 18:47:57 -0300

    feat: add first tab content component

--- src/views/modules/user/components/profileDetails/index.tsx
+++ src/views/modules/user/components/profileDetails/index.tsx
@@ -3,39 +3,24 @@ import TabContext from '@mui/lab/TabContext'
 import TabList from '@mui/lab/TabList'
 import TabPanel from '@mui/lab/TabPanel'
 import Box from '@mui/material/Box'
-import Button from '@mui/material/Button'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import CardHeader from '@mui/material/CardHeader'
 import Divider from '@mui/material/Divider'
 import Grid from '@mui/material/Grid'
 import Tab from '@mui/material/Tab'
-import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
 
 // ** Icon Imports
 import { Icon } from '@iconify/react'
-import { useFormik } from 'formik'
-import { TextInput } from 'src/@core/components/inputs'
 import { profileTabsData } from '../../data/profileTabsData'
 import useHandleTabChange from '../../hooks/useHandleTabChange'
 import { ProfileTabs } from '../../models/enum/ProfileTabs'
 import FileUpload from '../profilePictureInput'
+import FirstTabContent from '../profileTabs/firstTabContent'
 
 const ProfileDetails = () => {
   const { activeTabIndex, handleChange } = useHandleTabChange()
-
-  const validation = useFormik({
-    enableReinitialize: true,
-    initialValues: {
-      firstName: '',
-      lastName: '',
-      email: ''
-    },
-    onSubmit: values => {
-      console.log(values)
-    }
-  })
 
   const handleFileChange = (file: File) => {
     console.log('Uploaded file:', file)
@@ -70,41 +55,7 @@ const ProfileDetails = () => {
               })}
             </TabList>
             <TabPanel value='1'>
-              <form>
-                <CardContent>
-                  <Grid container spacing={6}>
-                    <Grid item xs={12} sm={6}>
-                      <TextInput
-                        inputIdentifier='firstName'
-                        inputLabel='Nome'
-                        onChange={validation.handleChange}
-                        onBlur={validation.handleBlur}
-                        value={validation.values.firstName}
-                      />
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                      <TextInput
-                        inputIdentifier='lastName'
-                        inputLabel='Sobremome'
-                        onChange={validation.handleChange}
-                        onBlur={validation.handleBlur}
-                        value={validation.values.lastName}
-                      />
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                      <TextField disabled fullWidth type='email' label='Email' value={validation.values.email} />
-                    </Grid>
-
-                    <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'end', mt: 5 }}>
-                      <Button type='reset' variant='outlined' color='secondary' sx={{ mr: 3 }}>
-                        Cancelar
-                      </Button>
-
-                      <Button variant='contained'>Salvar</Button>
-                    </Grid>
-                  </Grid>
-                </CardContent>
-              </form>
+              <FirstTabContent />
             </TabPanel>
             <TabPanel value='2'>
               <Typography>
