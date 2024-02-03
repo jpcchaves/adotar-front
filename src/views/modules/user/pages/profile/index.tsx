89164103325cfca7ce9d4f792ce20229bc27859d
João Chaves <joao.chaves@liferay.com>
2024-02-02 21:44:52 -0300

    feat: add base structure for user profile details apge

---
+++ src/views/modules/user/pages/profile/index.tsx
@@ -0,0 +1,11 @@
+import ProfileDetails from '../../components/profileDetails'
+
+const UserProfile = () => {
+  return (
+    <>
+      <ProfileDetails />
+    </>
+  )
+}
+
+export default UserProfile
