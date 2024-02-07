e6d12ec97ecb285cc6fea3019bbb131f0195ccf4
João Chaves <joao.chaves@liferay.com>
2024-02-06 21:51:05 -0300

    feat: add address response dto

---
+++ src/domain/DTO/address/AddressResponseDTO.ts
@@ -0,0 +1,10 @@
+export type AddressResponseDTO = {
+  id: string
+  zipcode: string
+  street: string
+  number: string
+  complement: string
+  neighborhood: string
+  city: string
+  state: string
+}
