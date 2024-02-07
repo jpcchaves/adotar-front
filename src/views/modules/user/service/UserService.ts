6aae6a27e59b43a7d9b75b498f9c0117ce9b358a
João Chaves <joao.chaves@liferay.com>
2024-02-06 21:51:21 -0300

    feat: add create/update/read methods for user address

--- src/views/modules/user/service/UserService.ts
+++ src/views/modules/user/service/UserService.ts
@@ -1,6 +1,11 @@
+import { AddressRequestDTO } from 'src/domain/DTO/address/AddressRequestDTO'
+import { AddressResponseDTO } from 'src/domain/DTO/address/AddressResponseDTO'
 import { UpdatePasswordDTO } from 'src/domain/DTO/auth/UpdatePasswordDTO'
 import { ApiMessageResponse } from 'src/domain/models/ApiMessageResponse'
 
 export interface UserService {
   updateUserPassword: (requestDTO: UpdatePasswordDTO) => Promise<ApiMessageResponse>
+  createUserAddress: (requestDTO: AddressRequestDTO) => Promise<ApiMessageResponse>
+  updateUserAddress: (requestDTO: AddressRequestDTO) => Promise<ApiMessageResponse>
+  getUserAddress: () => Promise<AddressResponseDTO>
 }
--- src/views/modules/user/service/UserServiceImpl.ts
+++ src/views/modules/user/service/UserServiceImpl.ts
@@ -1,3 +1,5 @@
+import { AddressRequestDTO } from 'src/domain/DTO/address/AddressRequestDTO'
+import { AddressResponseDTO } from 'src/domain/DTO/address/AddressResponseDTO'
 import { UpdatePasswordDTO } from 'src/domain/DTO/auth/UpdatePasswordDTO'
 import { ApiMessageResponse } from 'src/domain/models/ApiMessageResponse'
 import { HttpMethod, httpRequest } from 'src/utils/http'
@@ -7,6 +9,42 @@ class UserServiceImpl implements UserService {
   updateUserPassword = async (requestDTO: UpdatePasswordDTO): Promise<ApiMessageResponse> => {
     return new Promise((resolve, reject) => {
       httpRequest<UpdatePasswordDTO, ApiMessageResponse>(HttpMethod.PUT, '/v1/auth/update-password', requestDTO)
+        .then(res => {
+          resolve(res)
+        })
+        .catch(err => {
+          reject(err)
+        })
+    })
+  }
+
+  createUserAddress = (requestDTO: AddressRequestDTO): Promise<ApiMessageResponse> => {
+    return new Promise((resolve, reject) => {
+      httpRequest<AddressRequestDTO, ApiMessageResponse>(HttpMethod.POST, '/v1/addresses', requestDTO)
+        .then(res => {
+          resolve(res)
+        })
+        .catch(err => {
+          reject(err)
+        })
+    })
+  }
+
+  updateUserAddress = (requestDTO: AddressRequestDTO): Promise<ApiMessageResponse> => {
+    return new Promise((resolve, reject) => {
+      httpRequest<AddressRequestDTO, ApiMessageResponse>(HttpMethod.PUT, '/v1/addresses', requestDTO)
+        .then(res => {
+          resolve(res)
+        })
+        .catch(err => {
+          reject(err)
+        })
+    })
+  }
+
+  getUserAddress = (): Promise<AddressResponseDTO> => {
+    return new Promise((resolve, reject) => {
+      httpRequest<void, AddressResponseDTO>(HttpMethod.GET, '/v1/addresses')
         .then(res => {
           resolve(res)
         })
